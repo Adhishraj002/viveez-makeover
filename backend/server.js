1da0ef333d53e95613c71e1c418d3d66dbb2d6cf
Adhish Raj <adhishraj22@gmail.com>
2026-01-08 15:17:55 +0530

    update for email

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 const client = SibApiV3Sdk.ApiClient.instance;
-client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
+client.authentications["api-key"].apiKey = process.env.BREVO_SMTP_KEY;
 
 
 
